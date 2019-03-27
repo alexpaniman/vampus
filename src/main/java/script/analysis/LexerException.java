5aba7be508b9cd19c9d9383f8cd03d44a234b746
AlexPaniman <alexpaniman@gmail.com>
2019-03-27 02:43:48 +0200

    remove interpreter from project

--- src/main/java/script/analysis/LexerException.java
+++
@@ -1,8 +0,0 @@
-package script.analysis;
-
-@SuppressWarnings({"unused", "UnusedReturnValue", "WeakerAccess"})
-public class LexerException extends Exception {
-    public LexerException(String message) {
-        super(message);
-    }
-}
